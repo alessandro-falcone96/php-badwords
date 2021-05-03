26041db83d77d8dcacd1f9039abcac6241094048
Alessandro Falcone <alessandro.falcone@ymail.com>
2021-05-03 15:05:25 +0200

    Aggiunto index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP Badwords</title>
+  </head>
+  <body>
+    <H1>CIAO</H1>
+  </body>
+</html>
